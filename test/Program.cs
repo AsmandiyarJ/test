cc837f4a69b3d23af5ff77762ec8f71454c60509
Asmandiyar Jumaliev <jasmandiyar@gmail.com>
2023-12-18 20:19:23 +0600

    Добавьте файлы проекта.

---
+++ test/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Threading.Channels;
+using test;
+
+PublicClass publicClass = new PublicClass();
+List<PublicStruct> = new PublicStruct(); 
+
+string? hello ;
+hello = "hello";
+string World = hello; // задание 7 
+const int Kek = 4;
+string input = Console.ReadLine();
+Console.WriteLine(input);
+string reference;
+int? value;
+value = int.Parse(Console.ReadLine());
+if (value == 0)
+{
+    Console.WriteLine("другое число не мог загадать");
+}
+else
+{
+    Console.WriteLine("мм да уж ");
+}
+
+switch (input)
+{
+    case "1":
+        Console.WriteLine("sdvfsdvf");
+        break;
+    case "выавыва":
+        Console.WriteLine("sdvfsdvf");
+        break;
+    default: Console.WriteLine("sfdgnkjhgkr");
+            break;
+
+}
+// 16
+//17
+for(int i = 0; ;)
+{
+    i++;
+    Console.WriteLine("sdfasfas");
+}
+//18
+for(int i = 0;i <10 ;i++)
+{
+    Console.WriteLine("test");
+}
+//19
+for (int i = 1; i <= 100; i++)
+{
+    Console.WriteLine(i);
+}
+//20
+do
+{
+    Console.WriteLine("Hello");
+}
+while (false);
+//21
+int[] array = { 1, 2, 3, 4, 5 };
+//22
+foreach (int i in array)
+{
+    Console.WriteLine(i);
+}
+//23
+Console.WriteLine(array.Length);
+//24
+DateTime now = DateTime.Now;
+//25 не помню как надо было
+
+//26
+//double concat = $"{now} + {input}";// не помню жаль нельзя гуглить 
+//27
+//28
+//29
+//30
+
+//31 
+
+
+
+
+
+
+
+
+
+
---
+++ test/PublicClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//31
+namespace test
+{
+    public class PublicClass
+    {
+
+        public PublicClass()
+        {
+            Name = "DefaultName";
+            //41
+            Print(Name);
+
+        }
+        public string Name { get; set; }
+
+        private string? field = "your name";
+
+        public string Field
+        {
+            get { return field ?? string.Empty; }
+            set { field = value; }
+        }
+        //38
+        public void VoidMethod()
+        {
+            //42
+            int resuilt = 0;
+            IntMethod(resuilt);
+
+        }
+        //48
+        public void VoidMethod(int aX)
+        {
+            //42
+            int resuilt = 0;
+            IntMethod(resuilt);
+
+        }
+
+        //39
+        private int IntMethod(int ade)
+        {
+            return ade;
+        }
+        //40
+        public static void Print(string print)
+        {
+            Console.WriteLine(print);
+        }
+    }
+
+}
+
